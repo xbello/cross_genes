487db2d33a1671a1274dc05d3a5443e9e62a7365
Xabi Bello <xbello@gmail.com>
2016-07-22 18:37:59 +0200

    Added two file cross match.

--- cross_genes.py
+++ cross_genes.py
@@ -10,3 +10,25 @@ def cross_multiple(*gene_sets):
 def cross_two(first, second):
     """Return the common genes between two sets of genes."""
     return list(set(first) & set(second))
+
+
+def cross_two_files(filename, filename2):
+    """Return a list with the common genes between two archives.
+
+    Some gene names are multiple, like "ABCD, ABCE_1", and only the first one
+    should be considered.
+
+    """
+    with open(filename) as f1, open(filename2) as f2:
+        genes_first = []
+        genes_second = []
+        for line in f1:
+            gene = line.rstrip().replace('"', "").split(";")[0]
+            genes_first.append(gene)
+        for line in f2:
+            gene = line.rstrip().replace('"', "").split(";")[0]
+            genes_second.append(gene)
+
+    print(genes_first)
+
+    return cross_two(genes_first, genes_second)
--- test_cross_genes.py
+++ test_cross_genes.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from cross_genes import cross_multiple, cross_two
+from cross_genes import cross_two_files
 
 
 class TestCrossGenes(TestCase):
@@ -8,6 +9,10 @@ class TestCrossGenes(TestCase):
         self.genes_1 = ["GENE1", "GENE2", "GENE3"]
         self.genes_2 = ["GENE1", "GENE2", "GENE4"]
         self.genes_3 = ["GENE1", "GENE4", "GENE5"]
+
+        self.filename1 = "test_files/genes_list.txt"
+        self.filename2 = "test_files/genes_list2.txt"
+        self.filename3 = "test_files/genes_list3.txt"
 
     def test_cross_two(self):
         self.assertCountEqual(cross_two(self.genes_1, self.genes_2),
@@ -20,3 +25,9 @@ class TestCrossGenes(TestCase):
                                              self.genes_2,
                                              self.genes_3),
                               ["GENE1"])
+
+    def test_cross_two_files(self):
+        self.assertCountEqual(cross_two_files(self.filename1, self.filename2),
+                              ["NOC2L", "PCDHA9", "NOP16"])
+        self.assertCountEqual(cross_two_files(self.filename1, self.filename3),
+                              ["PCDHA9", "NOP16"])
