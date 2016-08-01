f8b2c97947b6c422e60401d7b9cf4045fc154936
Xabi Bello <xbello@gmail.com>
2016-08-01 14:28:13 +0200

    Simplified the tests.

--- test_cross_genes.py
+++ test_cross_genes.py
@@ -3,10 +3,6 @@ from unittest import TestCase
 
 import cross_genes as cg
 
-from cross_genes import cross_two
-from cross_genes import cross_combine, cross_multiple_files
-from cross_genes import load_list
-
 
 class TestCrossGenes(TestCase):
     def assertCountItemsEqual(self, *args, **kwargs):
@@ -28,37 +24,37 @@ class TestCrossGenes(TestCase):
         self.filename3 = join(path, "test_files/genes_list3.txt")
 
     def test_cross_two(self):
-        self.assertCountItemsEqual(cross_two(self.genes_1, self.genes_2),
+        self.assertCountItemsEqual(cg.cross_two(self.genes_1, self.genes_2),
                                    ["GENE1", "GENE2"])
-        self.assertCountItemsEqual(cross_two(self.genes_1, self.genes_3),
+        self.assertCountItemsEqual(cg.cross_two(self.genes_1, self.genes_3),
                                    ["GENE1"])
 
     def test_cross_multiple_files(self):
         self.assertCountItemsEqual(
-            cross_multiple_files(self.filename1,
-                                 self.filename2,
-                                 self.filename3),
+            cg.cross_multiple_files(self.filename1,
+                                    self.filename2,
+                                    self.filename3),
             ["PCDHA9", "PCDHA1", "PCDHA2", "PCDHA8", "NOP16"])
 
     def test_cross_one_file(self):
         self.assertCountItemsEqual(
-            cross_multiple_files(self.filename1),
+            cg.cross_multiple_files(self.filename1),
             ["NOC2L", "SMYD2", "OR2T35", "PCDHA9", "PCDHA1", "PCDHA2",
              "PCDHA8", "NOP16"])
 
     def test_cross_two_files(self):
         self.assertCountItemsEqual(
-            cross_multiple_files(self.filename1, self.filename2),
+            cg.cross_multiple_files(self.filename1, self.filename2),
             ["NOC2L", "PCDHA1", "PCDHA2", "PCDHA8", "PCDHA9", "NOP16"])
         self.assertCountItemsEqual(
-            cross_multiple_files(self.filename1, self.filename3),
+            cg.cross_multiple_files(self.filename1, self.filename3),
             ["PCDHA9", "PCDHA1", "PCDHA2", "PCDHA8", "NOP16"])
 
     def test_cross_combine_multiple_files(self):
         # When comparing multiple files, the user want to know all-vs-all and
         #  one-vs-one individually.
         self.assertCountItemsEqual(
-            cross_combine(self.filename1, self.filename2, self.filename3),
+            cg.cross_combine(self.filename1, self.filename2, self.filename3),
             {"genes_list-genes_list2":
              ["NOC2L", "PCDHA1", "PCDHA2", "PCDHA8", "PCDHA9", "NOP16"],
              "genes_list2-genes_list3":
@@ -70,7 +66,7 @@ class TestCrossGenes(TestCase):
 
     def test_load_list(self):
         self.assertCountItemsEqual(
-            load_list(self.filename1),
+            cg.load_list(self.filename1),
             ["NOC2L", "SMYD2", "OR2T35", "PCDHA1", "PCDHA2", "PCDHA8",
              "PCDHA9", "NOP16"])
 
